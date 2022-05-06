7a8f4409a57c4ffb538a5535ac48ce5c47bf6504
daraujo-etg <daraujo@essextec.com>
2022-05-05 20:27:13 -0400

    Added basic Home page with image and buttons

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button, Image, TextInput} from "react-native-web";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Buttons = () => {
+    return (
+        <View style={styles.formSelect}>
+
+            <StatusBar style="auto" />
+            <Button
+                onPress={() => {}}
+                title={"Start Injury Form"}
+            />
+            <Button
+                onPress={() => {}}
+                title={"Start Auto Accident Form"}
+            />
+        </View>
+    );
+
 }
+
+const App = () => {
+
+    return (
+        <View style={[styles.container, {flexDirection: 'column'}]}>
+
+        <StatusBar style="auto"/>
+            <View style={[styles.header, {flex: 10}]}>
+                <Text style={styles.title}> Stanley Tree Incident Form</Text>
+                <Image style={{width: 150, height: 150}}
+                       source={require('./assets/STS-logo.png')}/>
+            </View>
+            <View style={{flex: 2}}>
+                <Buttons />
+            </View>
+        </View>
+    );
+}
+
+
+export default  App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#706f6f',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#dddddd',
+
   },
+  header: {
+
+      alignItems: 'center',
+      justifyContent: 'top'
+  },
+  title: {
+      fontSize: 30,
+      color: 'red',
+  } ,
+    formSelect: {
+      justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
