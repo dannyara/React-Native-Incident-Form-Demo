34159fbc86011caffa8c66ac0a8ce6a01b7462a8
daraujo-etg <daraujo@essextec.com>
2022-05-10 15:42:27 -0400

    Added Navigation pages to home page

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import * as React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {Button, Image, TextInput} from "react-native-web";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Buttons = () => {
+const Stack = createNativeStackNavigator();
+
+const FormButtons = () => {
     return (
-        <View style = {styles.formSelect}>
-            <Text style = {{fontSize: 18}}> Select an incident form below: </Text>
+        <View style={styles.formSelect}>
+            <Text style={[styles.title, {fontSize: 18}]}> Select an incident form below: </Text>
             <Button
-                onPress = {() => {}}
-                title = {"Start Injury Form"}
+                onPress={() => {
+                    alert('You tapped the button!');
+                }}
+                title={"Start Injury Form"}
             />
             <Button
-                onPress={() => {}}
+                onPress={() => {
+                    alert('You tapped the button!');
+                }}
                 title={"Start Auto Accident Form"}
             />
+        </View>
+    );
+
+}
+const HomePage = () => {
+    return (
+        <View style={[styles.container, {flexDirection: 'column'}]}>
+
+            <StatusBar style="auto"/>
+            <View style={[styles.header, {flex: 10}]}>
+                <Image style={{width: 150, height: 150}}
+                       source={require('./assets/STS-logo.png')}/>
+            </View>
+            <View style={{flex: 2}}>
+                <FormButtons/>
+            </View>
         </View>
     );
 
@@ -22,41 +47,41 @@ const Buttons = () => {
 const App = () => {
 
     return (
-        <View style={[styles.container, {flexDirection: 'column'}]}>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomePage}
+                    options={{title: "Welcome to STS Connect"}}
+                />
 
-        <StatusBar style="auto"/>
-            <View style={[styles.header, {flex: 10}]}>
-                <Text style={styles.title}> Stanley Tree Incident Form</Text>
-                <Image style={{width: 150, height: 150}}
-                       source={require('./assets/STS-logo.png')}/>
-            </View>
-            <View style={{flex: 2}}>
-                <Buttons />
-            </View>
-        </View>
+            </Stack.Navigator>
+
+        </NavigationContainer>
+
     );
 }
 
 
-export default  App
+export default App
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#dddddd',
+    container: {
+        flex: 1,
+        backgroundColor: '#dddddd',
 
-  },
-  header: {
+    },
+    header: {
 
-      alignItems: 'center',
-      justifyContent: 'top'
-  },
-  title: {
-      fontSize: 30,
-      color: 'red',
-  } ,
+        alignItems: 'center',
+        justifyContent: 'top'
+    },
+    title: {
+        fontSize: 30,
+        color: 'red',
+    },
     formSelect: {
-      justifyContent: 'center',
+        justifyContent: 'center',
         alignItems: 'center'
     }
 });
