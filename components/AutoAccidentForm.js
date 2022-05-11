d6c4cc1bebb8c4b6c09afd091eb366b10e715cb3
daraujo-etg <daraujo@essextec.com>
2022-05-11 17:20:56 -0400

    added generic fields for auto accident and injury form

--- components/AutoAccidentForm.js
+++ components/AutoAccidentForm.js
@@ -2,18 +2,31 @@ import {StyleSheet, Text, TextInput, View} from "react-native";
 import * as React from "react";
 import {useState} from "react";
 
-const AutoAccidentPage = () => {
+const NameInput = ({label}) => {
     const [text, setText] = useState('')
+    return (
+      <View style={styles.nameInput}>
+          <label>{label}</label>
+          <TextInput value={text}
+                     placeholder = "Enter text here"
+                     onChangeText={newText => setText(newText)}
+                     defaultValue={text}>
+          </TextInput>
+      </View>
+    );
+}
+
+const AutoAccidentPage = () => {
+
     return (
         <View style={styles.container}>
             <Text style={{fontSize: 48}}> Auto Accident Form</Text>
-
-            <label > hello</label>
-            <TextInput value={text}
-                       style={styles.nameInput}
-                       placeholder={"Enter Full Name" }>
-
-            </TextInput>
+            <NameInput label={"Name of Employee Involved"} />
+            <NameInput label={"Incident Location"} />
+            <NameInput label={"STS Vehicle Involved"} />
+            <NameInput label={"Crew Leader Name"} />
+            <NameInput label={"Direct Supervisor"} />
+            <NameInput label={"Stanley Tree Division"} />
 
         </View>
     );
@@ -23,10 +36,10 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#dddddd',
         alignItems: 'center',
-        justifyContent: 'space-between'
     },
     nameInput: {
-        fontSize: 20
+        fontSize: 20,
+        justifyContent: 'top'
     }
 });
 export default AutoAccidentPage
--- components/InjuryForm.js
+++ components/InjuryForm.js
@@ -1,10 +1,31 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, TextInput, View} from "react-native";
+import {useState} from "react";
+import * as React from "react";
 
+
+const NameInput = ({label}) => {
+    const [text, setText] = useState('')
+    return (
+        <View style={styles.nameInput}>
+            <label>{label}</label>
+            <TextInput value={text}
+                       placeholder = "Enter text here"
+                       onChangeText={newText => setText(newText)}
+                       defaultValue={text}>
+            </TextInput>
+        </View>
+    );
+}
 const InjuryPage = () => {
     return (
         <View style={styles.container}>
-
-            <Text> Injury Form</Text>
+            <Text style={{fontSize: 48}}> Injury Form</Text>
+            <NameInput label={"Name of Employee Involved"} />
+            <NameInput label={"Incident Location"} />
+            <NameInput label={"STS Vehicle Involved"} />
+            <NameInput label={"Crew Leader Name"} />
+            <NameInput label={"Direct Supervisor"} />
+            <NameInput label={"Stanley Tree Division"} />
 
         </View>
     );
@@ -16,5 +37,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
 
     },
+    nameInput: {
+        fontSize: 20,
+        justifyContent: 'top'
+    }
 });
 export default InjuryPage
