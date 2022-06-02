84b62ebaa73420396660a5e44d45401520534da0
daraujo-etg <daraujo@essextec.com>
2022-06-02 19:16:15 -0400

    added same code for auto accident

--- components/AutoAccidentForm.js
+++ components/AutoAccidentForm.js
@@ -1,198 +1,252 @@
-import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
+const fields = [
+    {
+        'label': 'Name of Employee Involved',
+        'placeholder': 'Enter Full Name',
+        'required': true,
+        'type': 'text-field',
+    },
+    {
+        'label': 'Incident Location',
+        'placeholder': 'Enter Address',
+        'required': true,
+        'height': 60,
+        'type': 'time-date',
+    },
+    {
+        'label': 'STS Vehicle Involved',
+        'placeholder': 'Enter STS Equipment Number',
+        'required': true,
+        'type': 'text-field',
+    },
+    {
+        'label': 'Please Describe the Details of the Incident',
+        'placeholder': 'Enter Incident Details',
+        'height': 100,
+        'required': true,
+        'type': 'text-field',
+    },
+
+    {
+        'label': 'Crew Leader Name',
+        'placeholder': 'Enter Full Name',
+        'required': true,
+        'type': 'text-field',
+    },
+    {
+        'label': 'Direct Supervisor',
+        'placeholder': 'Enter Full Name',
+        'required': true,
+        'type': 'text-field',
+    },
+    {
+        'label': 'Stanley Tree Division',
+        'placeholder': 'Enter Division Name',
+        'required': true,
+        'type': 'dropdown',
+    },
+    {
+        'label': 'Police Report Number',
+        'placeholder': 'Enter police report details',
+        'required': true,
+        'type': 'text-field',
+    },
+]
+
+import {
+    Button,
+    Keyboard,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
 import * as React from "react";
 import {useState} from "react";
 import RadioButtonRN from 'radio-buttons-react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
-
-// function to handle required field length, returns true if input is valid
-const validateLength = (required, text) => {
-    return (required || text.length > 0)
-}
-
-const DisplayFields = (input) => {
-    let fields = input.fields
-    let inputArray = input.userInput
-    //function for updating user input each time they type into the field
-    const updateInput = (text, index) => {
-        setUserInput(previousState => {
-            previousState.inputArr[index] = text
-            return { inputArr: previousState.inputArr}
-        })
-    }
-    function getValue(userInput, index) {
-        return userInput.inputArr[index]
-    }
-
-    const [userInput, setUserInput] = useState({
-        inputArr: inputArray,
-        fieldBeingEdited: null
-    })
-    // handle the border color
-    const handleBorderColor = (userInput,index) => {
-        // if text is larger than 0
-        if(validateLength(userInput.inputArr, index)){
-            return index === userInput.fieldBeingEdited ? 'green' : 'maroon'
-        }
-        return 'red'
-
-    }
-
-    return(
-        <View style={{width: '100%', alignItems: 'center'}}>
-            {/*//fields.map == for(field in fields...*/}
-
-            {fields.map((field, index) =>
-                // switch statement here, textinput, radio , datefields, dropdown
-                <View style={styles.fieldList}>
-                    <Text> {field.label} </Text>
-                    <TextInput
-                        key={index}
-                        value={getValue(userInput, index)}
-                        placeholder={field.placeholder}
-                        // conditional styling changes border color depending on user input
-                        style={[styles.textInput, {height: field.height || 50, borderColor: handleBorderColor(userInput,index), borderWidth: 1} ]}
-                        autoCapitalize='words'
-                        multiline={field.height && true}
-                        // keeps track of user input
-                        onChangeText={newText => {
-                            updateInput(newText, index)
-                        }}
-                        //onblur == when user clicks out of the field
-                        onBlur={() => {
-                            if (!validateLength(field.required, userInput.inputArr[index])) {
-                                alert("This is a Required Field")
-                            }
-                            setUserInput({...userInput, fieldBeingEdited: null})
-                        }}
-                        //on focus == user clicks into the field
-                        onFocus = { ()=> {
-                            setUserInput({...userInput, fieldBeingEdited: index})
-                        }}
-                    />
-                </View>
-            )}
-            <View style={{marginTop: 10, marginBottom: 20}}>
-                <Text style={{fontSize: 16, textAlign: 'center'}}> Police Report Made?</Text>
-                <View style={{flexDirection: 'row', width: '80%'}}>
-                    <RadioButtonRN
-                        data={[{'label': 'Yes'}, {'label': 'No'}]}
-                        style={{flex: 1}}
-                        selectedBtn={(e) => console.log(e)}
-                        circleSize={20}
-                        icon={
-                            <Icon
-                                name="check-circle"
-                                size={25}
-                                color="#bc7d9c"
-                            />
-                        }
-                    />
-                </View>
-            </View>
-        </View>
-
-    )
-}
-
+import COLORS from "../assets/colors";
+import FieldInput from "./FieldInput";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AutoAccidentForm = ({navigation}) => {
     // json object of fields to be used in the form
-    const fields = [
-        {
-            'label': 'Name of Employee Involved',
-            'placeholder': 'Enter Full Name',
-            'required': true,
-            'type': 'text-field',
-        },
-        {
-            'label': 'Incident Location',
-            'placeholder': 'Enter Address',
-            'required': true,
-            'height': 60,
-            'type': 'time-date',
-        },
-        {
-            'label': 'STS Vehicle Involved',
-            'placeholder': 'Enter STS Equipment Number',
-            'required': true,
-            'type': 'text-field',
-        },
-        {
-            'label': 'Please Describe the Details of the Incident',
-            'placeholder': 'Enter Incident Details',
-            'height': 100,
-            'required': true,
-            'type': 'text-field',
-        },
+    const [errors, setErrors] = useState({})
+    const validate = () => {
+        Keyboard.dismiss()
+        //valid variable for live style validation
+        let valid = true
+        setErrors({})
+        if (!input.employeeName) { //if employee name is empty
+            handleError('name', 'This is a required field')
+            valid = false
+        } else if (!input.employeeName.match(/\w{2,}\s\w{2,}/)) { // if name isn't first + last name
+            handleError('name', 'Please enter a full name, ie. John Doe')
+            valid = false
+        }
+        if (!input.incidentLocation) {
+            handleError('location', 'This is a required field')
+            valid = false
+        }
+        if (!input.incidentDetails) {
+            handleError('incidentDetails', 'This is a required field, please provide all details')
+            valid = false
+        }
 
-        {
-            'label': 'Crew Leader Name',
-            'placeholder': 'Enter Full Name',
-            'required': true,
-            'type': 'text-field',
-        },
-        {
-            'label': 'Direct Supervisor',
-            'placeholder': 'Enter Full Name',
-            'required': true,
-            'type': 'text-field',
-        },
-        {
-            'label': 'Stanley Tree Division',
-            'placeholder': 'Enter Division Name',
-            'required': true,
-            'type': 'dropdown',
-        },
-        {
-            'label': 'Police Report Number',
-            'placeholder': 'Enter police report details',
-            'required': true,
-            'type': 'text-field',
-        },
-    ]
+        if (!input.crewLeaderName) {
+            handleError('crewLeaderName', 'This is a required field')
+            valid = false
+        }
+        if (!input.incidentDateTime) {
+            handleError('incidentDateTime', 'This is a required field')
+            valid = false
+        }
+        if (!input.vehicleInvolved) {
+            handleError('vehicleInvolved', 'This is a required field')
+            valid = false
+        }
+        if (!input.policeReportMade) {
+            handleError('policeReportMade', 'This is a required field')
+            valid = false
+        }
+        if (valid) {
+            submit().then(r => {
+                Keyboard.dismiss()
+                alert('Auto Accident Form submitted successfully!')
+                console.log(r)
+                navigation.pop()
+            })
+        }
+    }
+    const submit = async () => {
+        try {
+            const val = JSON.stringify(input, 0, 2)
+            console.log(val)
+            await AsyncStorage.setItem('AutoAccidentForm', val)
+        } catch (e) {
+            alert('an unexpected error has occurred:', '\n', e)
+        }
+    }
 
-
-    // create empty array for capturing user input
-    const inputArray = Array(10).fill('')
+    const [input, setInput] = useState({
+        employeeName: '',
+        incidentLocation: '',
+        incidentDetails: '',
+        crewLeaderName: '',
+        division: '',
+        supervisorName: '',
+        incidentDateTime: '',
+        vehicleInvolved: '',
+        policeReportMade: false,
+    })
+    //This function handles updating the user input object, takes the field name and the text to update
+    const handleOnChange = (field, text) => {
+        setInput(previousState => {
+            return {...previousState, [field]: text} // square brackets because of dynamic field, see https://stackoverflow.com/questions/43638938/updating-an-object-with-setstate-in-react
+        })
+    }
+    const handleError = (field, errMsg) => {
+        console.log(field, errMsg)
+        setErrors(previousState => ({...previousState, [field]: errMsg}))
+        // square brackets because of dynamic field, see https://stackoverflow.com/questions/43638938/updating-an-object-with-setstate-in-react
+    }
 
     return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 36}}> Auto Accident Form</Text>
-            <DisplayFields fields={fields} userInput={inputArray}/>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <Text style={styles.label}> Auto Accident Form</Text>
             <View>
-                <Button
-                    title="Submit"
-                    onPress={() => {
+                <FieldInput
+                    label='Name of Employee Involved'
+                    placeholder='Enter full Name'
+                    error={errors.name}
+                    onChangeText={(text) => handleOnChange('employeeName', text)}
+                />
+                <FieldInput
+                    label='Location of Incident'
+                    placeholder='Please include address, town, state, and zip'
+                    error={errors.location}
+                    multiline={true}
+                    height={60}
+                    onChangeText={(text) => handleOnChange('incidentLocation', text)}
+                />
+                <FieldInput
+                    label='Date and time of Incident'
+                    placeholder='Pmm/dd/yy - hh:mmPM/AM'
+                    error={errors.incidentDateTime}
+                    onChangeText={(text) => handleOnChange('incidentDateTime', text)}
+                />
 
-                        console.log(fields.label, inputArray)
-                        alert(inputArray)
-                        navigation.pop()
+                <FieldInput
+                    label='Crew Leader Name'
+                    placeholder='Enter the full name of the crew leader'
+                    error={errors.crewLeaderName}
+                    onChangeText={(text) => handleOnChange('crewLeaderName', text)}
+                />
+                <FieldInput
+                    label='Direct Supervisor'
+                    placeholder='Enter full name of the supervisor'
+                    error={errors.supervisorName}
+                    onChangeText={(text) => handleOnChange('crewLeaderName', text)}
+                />
+                <FieldInput
+                    label='Stanley Tree Division'
+                    placeholder='Enter Division name'
+                    error={errors.division}
+                    onChangeText={(text) => handleOnChange('division', text)}
+                />
+                <FieldInput
+                    label='Incident Details'
+                    placeholder='Please explain in detail how the accident occurred'
+                    error={errors.incidentDetails}
+                    height={120}
+                    multiline={true}
+                    onChangeText={(text) => handleOnChange('incidentDetails', text)}
+                />
+                <FieldInput
+                    label='Police Report Made'
+                    placeholder='Please enter all that apply'
+                    error={errors.policeReportMade}
+                    onChangeText={(text) => handleOnChange('policeReportMade', text)}
+                />
+                <FieldInput
+                    label='STS Vehicle Involved'
+                    placeholder='Please enter all that apply'
+                    error={errors.vehicleInvolved}
+                    onChangeText={(text) => handleOnChange('vehicleInvolved', text)}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title={"Submit"}
+                    onPress={() => {
+                        validate()
                     }}
                 />
             </View>
-        </View>
+
+        </ScrollView>
     )
 }
 
 const AutoAccidentPage = ({navigation}) => {
     return (
-        <ScrollView style={styles.scrollContainer}>
+        <SafeAreaView style={styles.container}>
             <AutoAccidentForm navigation={navigation}/>
-
-        </ScrollView>
+        </SafeAreaView>
     );
 }
+
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#dddddd',
-        alignItems: 'center',
-        padding: 20,
-
+        backgroundColor: COLORS.white,
+        flex: 1
     },
     scrollContainer: {
         flex: 1,
+        paddingTop: 50,
+        paddingBottom: 20,
+        paddingHorizontal: 25,
     },
     fieldList: {
         fontSize: 20,
@@ -201,8 +255,14 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     buttonContainer: {
-        margin: 20,
-        width: '40%'
+        padding: 20
+    },
+    label: {
+        fontSize: 32,
+        color: COLORS.black,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 25,
     },
     textInput: {
         backgroundColor: 'white',
@@ -211,6 +271,16 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'maroon',
         borderStyle: 'solid',
+    },
+    submitButton: {
+        height: 55,
+        width: '40%',
+        margin: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: COLORS.offWhite,
+        fontWeight: 'bold',
+        fontSize: 18
     }
 });
 export default AutoAccidentPage
